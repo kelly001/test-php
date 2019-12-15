918fccf261531ec3ad0c4ad2f1e9ebb5faec4088
yzhuravleva <yzhuravleva@mmtr.ru>
2019-12-15 21:09:27 +0300

    controller

---
+++ lib/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Z570
+ * Date: 15.12.2019
+ * Time: 21:03
+ */
+
+namespace Controllers;
+
+use Models\User;
+use Models\Group;
+
+class UserController
+{
+    public  function registration($_REQUEST){
+
+    }
+
+    public function login($_REQUEST){
+
+    }
+
+    public function getInfo($user_id) {
+
+    }
+
+
+
+}
